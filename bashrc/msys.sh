cdf3bf393f8012c9320013b375fe29dfeaf8473e
74th <site@j74th.com>
2016-05-04 10:48:54 +0900

    golangの設定とか追加

--- bashrc/msys.sh
+++ bashrc/msys.sh
@@ -1,8 +1,27 @@
 # MSYS2用PATHの追加設定
 
 # golang
-export PATH=$PATH:/c/tools/go/bin/
+if [ -e /c/tools/go/bin ];then
+	export PATH=$PATH:/c/tools/go/bin
+fi
 
 # docker
-export PATH=$PATH:/c/Program\ Files/Docker/Docker/Resources/bin/
+if [ -e /c/Program\ Files/Docker/Docker/Resources/bin/ ];then
+	export PATH=$PATH:/c/Program\ Files/Docker/Docker/Resources/bin
+	alias docker='winpty docker'
+	alias docker-compose='winpty docker-compose'
+fi
 
+# nodejs
+if [ -e /c/Program\ Files/nodejs/ ];then
+	export PATH=$PATH:/c/Program\ Files/nodejs
+	alias node='winpty node'
+	alias npm='winpty npm'
+fi
+
+# vim
+if [ -e /c/Program\ Files\ \(x86\)/vim/vim74/ ];then
+	alias vi='winpty /c/Program\ Files\ \(x86\)/vim/vim74/vim.exe'
+	alias vim='winpty /c/Program\ Files\ \(x86\)/vim/vim74/vim.exe'
+	alias gvim='/c/Program\ Files\ \(x86\)/vim/vim74/gvim.exe'
+fi
