bf01e4b240df2f7dafafc984255681c8ccba82bd
74th <site@j74th.com>
2016-05-06 11:41:35 +0900

    atomを使うのをやめた

--- atom/CreateLink.sh
+++
@@ -1,7 +0,0 @@
-ln -s ~/dotfiles/atom/config.cson ~/.atom/config.cson
-ln -s ~/dotfiles/atom/init.coffee ~/.atom/init.coffee
-ln -s ~/dotfiles/atom/keymap.cson ~/.atom/keymap.cson
-ln -s ~/dotfiles/atom/macros.coffee ~/.atom/macros.coffee
-ln -s ~/dotfiles/atom/projects.cson ~/.atom/projects.cson
-ln -s ~/dotfiles/atom/snippets.cson ~/.atom/snippets.cson
-ln -s ~/dotfiles/atom/styles.less ~/.atom/styles.less
