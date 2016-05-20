92a5ca944fb17e62f118d3f243c0152550ae8822
74th <site@j74th.com>
2016-05-20 13:26:53 +0900

    windowsではファイル属性を無視

---
+++ git/set-config.bat
@@ -0,0 +1,5 @@
+git config --global user.name 74th
+git config --global user.email site@j74th.com
+git config --global core.autocrlf input
+git config --global core.safecrlf true
+git config --global core.filemode false
