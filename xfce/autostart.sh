e8ac0d0046642bed20215136e87ba8cb53d7aa1b
74th <site@j74th.com>
2016-06-02 08:06:01 +0900

    xfceにて自動起動するコマンド

---
+++ xfce/autostart.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+# xfce4で自動起動するコマンド
+# Applications Menu -> Settings -> Settings Manager -> Session and Startup -> Application Autostart にて設定すること
+
+# キーの入れ替え
+xmodmap /home/atsushi/dotfiles/xmodmap/.xmodmap
