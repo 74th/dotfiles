e3484592796530c860f2083387c94ace6c493dbf
74th <site@j74th.com>
2016-06-05 09:21:46 +0900

    Windowsの場合はファイルモードを無視すること

--- git/set-config.sh
+++ git/set-config.sh
@@ -3,6 +3,7 @@ git config --global core.safecrlf true
 
 git config --global push.default current
 
+# 人間らしいGitコマンド
 git config --global alias.branches "branch -a"
 git config --global alias.tags "tag"
 git config --global alias.stashes "stash list"
@@ -10,6 +11,10 @@ git config --global alias.unstage "reset -q HEAD --"
 git config --global alias.discard "checkout --"
 git config --global alias.uncommit "reset --mixed HEAD~"
 git config --global alias.amend "commit --amend"
-git config --global alias.graph "log --graph -10 --branches --remotes --tags  --format=format:'%Cgreen%h %Creset• %<(75,trunc)%s (%cN, %cr) %Cred%d' --date-order"
+git config --global alias.graph "log --graph -20 --branches --remotes --tags  --format=format:'%Cgreen%h %Creset• %<(75,trunc)%s (%cN, %cr) %Cred%d' --date-order"
 git config --global alias.unmerged "diff --name-only --diff-filter=U"
 git config --global alias.history "log -10 --format=format:'%Cgreen%h %Creset• %s (%cN, %ar)'"
+
+# Windowsの場合、以下も追加する
+# ファイルモードを無視
+# git config --global core.filemode false
