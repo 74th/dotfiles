2891c633e8ac2865422c84de374ccb1f30bd4c49
74th <site@j74th.com>
2016-07-02 08:00:14 +0900

    ubuntuでdotnetのインストールに必要なものを追加

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,12 +1,26 @@
 sudo apt update
 
+# git
 if [ $# = 0 ] || [ $1 = "git" ];then
 	sudo apt install -y git git-flow bash-completion
 	sudo curl -o /etc/bash_completion.d/git-flow-completion.bash https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.bash
 fi
+
+# tmux
 if [ $# = 0 ] || [ $1 = "tmux" ];then
 	sudo apt install -y tmux
 fi
+
+# mozc
 if [ $# = 0 ] || [ $1 = "mozc" ];then
 	sudo apt install -y ibus-mozc
 fi
+
+# dotnet need
+sudo apt install apt-transport-https
+
+# font-manager
+if [ $# = 0 ] || [ $1 = "font-manager" ];then
+	sudo apt install -y font-manager
+fi
+
