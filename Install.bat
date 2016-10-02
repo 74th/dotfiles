0e0538320260a5870aa3c461ec48a11dbf1b7f41
74th <site@j74th.com>
2016-10-02 20:17:37 +0900

    windowのインストールをアップデート

--- Install.bat
+++ Install.bat
@@ -1,9 +1,3 @@
-REM vim
-IF EXIST %USERPROFILE%\.vimrc del %USERPROFILE%\.vimrc
-copy %USERPROFILE%\dotfiles\vimrc\rootvimrc.vim %USERPROFILE%\.vimrc
-IF EXIST %USERPROFILE%\.gvimrc del %USERPROFILE%\.gvimrc
-copy %USERPROFILE%\dotfiles\vimrc\rootgvimrc.vim %USERPROFILE%\.gvimrc
-
 REM bash
 IF EXIST %USERPROFILE%\.bashrc del %USERPROFILE%\.bashrc
 mklink /H %USERPROFILE%\.bashrc %USERPROFILE%\dotfiles\bashrc\bashrc 
@@ -16,3 +10,8 @@ IF EXIST %VSCODE_DIR%\settings.json del %VSCODE_DIR%\settings.json
 mklink /H %VSCODE_DIR%\settings.json %USERPROFILE%\dotfiles\vscode\settings.json
 IF EXIST %VSCODE_DIR%\snippets rmdir /S /Q %VSCODE_DIR%\snippets
 mklink /j %VSCODE_DIR%\snippets %USERPROFILE%\dotfiles\vscode\snippets
+
+REM msys2
+SET MSYS2_HOME=C:\msys64\home\%USERNAME%
+IF EXIST %MSYS2_HOME%\.minttyrc del %MSYS2_HOME%\.minttyrc
+mklink /H %MSYS2_HOME%\.minttyrc %USERPROFILE%\dotfiles\mintty\.minttyrc
