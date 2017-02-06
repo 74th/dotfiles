7d26b71aa975c3ee755200895d9014b323f15685
74th <site@j74th.com>
2017-02-06 15:42:15 +0900

    git addremove=git reset

--- git/set-config.sh
+++ git/set-config.sh
@@ -18,6 +18,8 @@ git config --global core.preloadindex true
 echo 人間らしいgitコマンド
 git config --global alias.branches "branch -a"
 git config --global alias.addline "add -p"
+git config --global alias.addlineedit "add -e"
+git config --global alias.addremove "reset"
 git config --global alias.tags "tag"
 git config --global alias.stashes "stash list"
 git config --global alias.unstage "reset -q HEAD --"
