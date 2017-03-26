032c9d937bcbc0d6b560ab4487c103da5b1edfa6
74th <site@j74th.com>
2017-03-26 15:16:28 +0900

    up

--- Install.bat
+++ Install.bat
@@ -11,7 +11,9 @@ mklink /H %VSCODE_DIR%\settings.json %USERPROFILE%\dotfiles\vscode\settings.json
 IF EXIST %VSCODE_DIR%\snippets rmdir /S /Q %VSCODE_DIR%\snippets
 mklink /j %VSCODE_DIR%\snippets %USERPROFILE%\dotfiles\vscode\snippets
 
-REM msys2
-SET MSYS2_HOME=C:\msys64\home\%USERNAME%
-IF EXIST %MSYS2_HOME%\.minttyrc del %MSYS2_HOME%\.minttyrc
-mklink /H %MSYS2_HOME%\.minttyrc %USERPROFILE%\dotfiles\mintty\.minttyrc
+REM Cygwin64
+SET CYGWIN_HOME=C:\cygwin64\home\%USERNAME%
+IF EXIST %CYGWIN_HOME%\.minttyrc del %MSYS2_HOME%\.minttyrc
+mklink /H %CYGWIN_HOME%\.minttyrc %USERPROFILE%\dotfiles\mintty\.minttyrc
+
+pause
