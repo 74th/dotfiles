b808be05d435d7534b12638eb2151d4bbd3d8685
74th <site@j74th.com>
2017-03-31 07:19:18 +0900

    peco_ls、ローカルの.config.fishを参照する

--- fish/config.fish
+++ fish/config.fish
@@ -191,3 +191,29 @@ if test $OSNAME = "Mac"
 	end
 end
 
+#--------------------------------------
+# peco
+function peco_ls
+	if test (count $argv) = 0
+		set peco_flags
+	else
+		set peco_flags --query "$argv"
+	end
+
+	history|peco $peco_flags|read foo
+
+	if [ $foo ]
+		commandline $foo
+	else
+		commandline ''
+	end
+end
+function fish_user_key_bindings
+	bind -M insert \cr 'peco_ls (commandline -b)'
+end
+
+#--------------------------------------
+# ローカル設定
+if test -e $HOME/.config.fish
+	source $HOME/.config.fish
+end
