c7f524e2ebcf413d45ab528482a3e931b6e44315
74th <site@j74th.com>
2017-04-25 16:41:03 +0900

    git graph http://blog.toshimaru.net/git-log-graph/

--- git/set-config.sh
+++ git/set-config.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo 改行コード対策
 git config --global core.autocrlf false
 echo CRLFをコミットしようとしたらエラー
@@ -26,7 +28,7 @@ git config --global alias.unstage "reset -q HEAD --"
 git config --global alias.discard "checkout --"
 git config --global alias.uncommit "reset --mixed HEAD~"
 git config --global alias.amend "commit --amend"
-git config --global alias.graph "log --graph -20 --branches --remotes --tags  --format=format:'%Cgreen%h %Creset• %<(75,trunc)%s (%cN, %cr) %Cred%d' --date-order"
+git config --global alias.graph "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 git config --global alias.unmerged "diff --name-only --diff-filter=U"
 git config --global alias.history "log -10 --format=format:'%Cgreen%h %Creset• %s (%cN, %ar)'"
 git config --global alias.deleteuntrackedfile "logi clean -f"
