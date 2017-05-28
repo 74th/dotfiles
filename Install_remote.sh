a05e0b2376b310dde68dd1cdb5ced43253664503
74th <site@j74th.com>
2017-05-28 17:32:06 +0900

    up

--- Install_remote.sh
+++ Install_remote.sh
@@ -11,7 +11,7 @@ scp ~/.ssh/id_rsa.pub $1:.ssh/authorized_keys
 scp ~/.ssh/server_id_rsa $1:.ssh/id_rsa
 scp ~/.ssh/server_id_rsa.pub $1:.ssh/id_rsa.pub
 
-ssh -t $1 "[ -e /etc/debian_version ] && sudo apt install git"
+ssh -t $1 "[ -e /etc/debian_version ] && sudo apt install git curl"
 
-ssh -t $1 git clone git@github.com:74th/dotfiles.git
+ssh -t $1 "git clone git@github.com:74th/dotfiles.git
 ssh $1 cd dotfiles\;./Install.sh
