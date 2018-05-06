acd23d90e183381d2341fc387b779eaa2984ee9a
74th <site@j74th.com>
2018-05-06 10:49:46 +0900

    up

--- backup_mac/backup.sh
+++ backup_mac/backup.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 set -Ceux
 
-OUTPUT_DIR=/Volumes/HDD/Backup/
+OUTPUT_DIR=/Volumes/MacHDD/backup
 
 function backup_dir() {
 	rm -rf ${OUTPUT_DIR}/${NAME}
 	if [ -e ${DIR} ]; then
-		tar -zcvf ${OUTPUT_DIR}/${NAME}.tar.gz ${DIR}
+		tar -cvf ${OUTPUT_DIR}/${NAME}.tar ${DIR}
 	fi
 }
 
 #NAME=Documents
 #DIR=~/Documents
 #backup_dir
-#
+
 #NAME=Desktop
 #DIR=~/Desktop
 #backup_dir
-#
+
 #NAME=go
 #DIR=~/go
 #backup_dir
-#
+
 #NAME=npm
 #DIR=~/npm
 #backup_dir
-#
+
 #NAME=vscode
 #DIR=~/.vscode
 #backup_dir
-#
-#NAME=.config
-#DIR=~/.config
-#backup_dir
-#
-#NAME=.ssh
-#DIR=~/.ssh
-#backup_dir
+
+NAME=config
+DIR=~/.config
+backup_dir
+
+NAME=ssh
+DIR=~/.ssh
+backup_dir
 
 #brew list > ${OUTPUT_DIR}/homebrew.txt
 
-ls -1 /Applications/ > ${OUTPUT_DIR}/Applications.txt
+#ls -1 /Applications/ > ${OUTPUT_DIR}/Applications.txt
