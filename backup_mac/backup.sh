0fc0055cc61fe5960fc35d45f66e77bee0a69c11
74th <site@j74th.com>
2018-05-06 10:50:32 +0900

    up

--- backup_mac/backup.sh
+++ backup_mac/backup.sh
@@ -10,25 +10,25 @@ function backup_dir() {
 	fi
 }
 
-#NAME=Documents
-#DIR=~/Documents
-#backup_dir
+NAME=Documents
+DIR=~/Documents
+backup_dir
 
-#NAME=Desktop
-#DIR=~/Desktop
-#backup_dir
+NAME=Desktop
+DIR=~/Desktop
+backup_dir
 
-#NAME=go
-#DIR=~/go
-#backup_dir
+NAME=go
+DIR=~/go
+backup_dir
 
-#NAME=npm
-#DIR=~/npm
-#backup_dir
+NAME=npm
+DIR=~/npm
+backup_dir
 
-#NAME=vscode
-#DIR=~/.vscode
-#backup_dir
+NAME=vscode
+DIR=~/.vscode
+backup_dir
 
 NAME=config
 DIR=~/.config
@@ -38,6 +38,6 @@ NAME=ssh
 DIR=~/.ssh
 backup_dir
 
-#brew list > ${OUTPUT_DIR}/homebrew.txt
+brew list > ${OUTPUT_DIR}/homebrew.txt
 
-#ls -1 /Applications/ > ${OUTPUT_DIR}/Applications.txt
+ls -1 /Applications/ > ${OUTPUT_DIR}/Applications.txt
