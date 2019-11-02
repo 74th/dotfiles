3726b1740b4a4eb4d5d4bf251adae32f39979d8d
74th <site@j74th.com>
2019-11-02 11:22:54 +0900

    same alias and path for bash and xonsh

---
+++ xonsh/xonsh_conf/aliases.py
@@ -0,0 +1,44 @@
+#!/usr/local/bin/python3
+import os
+from typing import Dict, List
+
+def get_aliases()->Dict[str,List[str]]:
+    a: Dict[str,List[str]] = {}
+    a["gt"] = ["git", "status"]
+    a["commit"] = ["git", "commit", "-v"]
+    a["add"] = ["git", "add"]
+    a["push"] = ["git", "push"]
+    a["pull"] = ["git", "pull"]
+
+    # kubernetes
+    a["k"] = ["kubectl"]
+    a["kg-pods"] = ["kubectl", "get", "pods", "--sort-by=.metadata.creationTimestamp"]
+    a["kg"] = ["kubectl", "get"]
+    a["kd"] = ["kubectl", "delete"]
+
+    # xonsh
+    a["xssh"] = ["ssh", "-t", "-c", "xonsh"]
+
+    # syntax_sugar
+    a["al"] = ["ls", "-al"]
+    a["la"] = ["ls", "-al"]
+    a["ll"] = ["ls", "-al"]
+    a["lt"] = ["ls", "-alt"]
+
+    # java
+    a["javac"] = ["javac", "-J-Dfile.encoding=utf-8"]
+    a["java"] = ["java", "-Dfile.encoding=UTF-8"]
+
+    return a
+
+def export_bash(aliases: Dict[str,List[str]]):
+    output = ""
+    for alias, cmd in aliases.items():
+        output += "alias " + alias + "='" + " ".join(cmd) + "';"
+    print(output)
+
+if __name__ == "__main__":
+    # for bash
+    # eval "$(python3 ~/dotfiles/xonsh/xonsh_conf/aliases.py)"
+    aliases = get_aliases()
+    export_bash(aliases)
---
+++ xonsh/xonsh_conf/path.py
@@ -0,0 +1,53 @@
+#!/usr/local/bin/python3
+import os
+from typing import List
+
+def get_paths(default_paths : List[str]) -> List[str]:
+
+    HOME = os.environ.get("HOME", "/home/nnyn/")
+    _paths: List[str] = []
+
+    def add(path: str):
+        if path not in default_paths and os.path.exists(path):
+            _paths.insert(0, path)
+
+    # Ubuntuなどで追加されてなかった時用
+    add("/usr/local/bin")
+    add("/usr/local/sbin")
+
+    # for Ubuntu
+    add("/snap/bin")
+
+    # Homebrew for Linux
+    add(f"/home/linuxbrew/.linuxbrew/bin")
+
+    add("/usr/local/cuda/bin")
+    add(f"{HOME}/Library/Python/2.7/bin")
+    add(f"{HOME}/Library/Python/3.7/bin")
+    add(f"{HOME}/google-cloud-sdk/bin")
+    add(f"{HOME}/google-cloud-sdk/platform/google_appengine")
+    add(f"{HOME}/npm/bin")
+    add(f"{HOME}/npm/node_modules/.bin")
+    add(f"{HOME}/Library/Android/sdk/platform-tools")
+    add(f"{HOME}/.rbx_env/shims")
+    add(f"{HOME}/.pyenv/shims")
+    add(f"{HOME}/go/bin")
+    add(f"{HOME}/dotfiles/bin/darwin")
+    add(f"{HOME}/go/src/github.com/uber/go-torch/FlameGraph")
+    add("/opt/X11/bin")
+    add("/usr/local/share/dotnet")
+    add("/Library/Frameworks/Mono.framework/Versions/Current/Commands")
+    add("/Library/TeX/texbin")
+
+    add(f"{HOME}/bin")
+    add(f"{HOME}/dotfiles/bin")
+
+    return _paths
+
+if __name__ == "__main__":
+    # for bash
+    # eval "$(python3 ~/dotfiles/xonsh/xonsh_conf/path.py)"
+    current_paths = os.environ["PATH"].split(":")
+    additional_paths = get_paths(current_paths)
+    if additional_paths:
+        print("export PATH=$PATH:" + ":".join(additional_paths))
