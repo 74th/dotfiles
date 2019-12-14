baa25d2f20ac9f6a50db73145f8103aad6192967
74th <site@74th.tech>
2019-12-15 08:00:23 +0900

    up

--- ubuntu/startup_miriam.sh
+++ ubuntu/startup_miriam.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-gsettings set org.gnome.desktop.interface scaling-factor 2
-xrandr --output DP-0 --scale 1.5x1.5
-xinput set-prop 'Logitech Rechargeable Touchpad T650' 'Trackpad Scroll Buttons' 5 4 6 7
-xinput set-prop 'Logitech Rechargeable Touchpad T650' 'Trackpad Drag Settings' 0 350 40 200
-xinput set-prop 'Logitech Rechargeable Touchpad T650' 'Trackpad Scroll Distance' 30
+sleep 3
+xrandr --output HDMI-0 --scale 1.5x1.5
