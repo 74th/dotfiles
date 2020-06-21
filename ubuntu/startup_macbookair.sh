0fbf1ac41d22b9dd498270694ad3bc4eb872701c
74th <site@74th.tech>
2020-06-21 14:13:48 +0900

    up

--- ubuntu/startup_macbookair.sh
+++ ubuntu/startup_macbookair.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+sleep 3
 echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
 echo 1 | sudo tee /sys/module/hid_apple/parameters/swap_opt_cmd
+xinput set-prop 'bcm5974' 'libinput Tapping Drag Enabled' 0
