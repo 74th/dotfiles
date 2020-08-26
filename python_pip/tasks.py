231e6ddf9701b27c2c811fc4e41eeb097658f755
Atsushi Morimoto (@74th) <site@74th.tech>
2020-08-26 21:31:34 +0900

    colorize xonsh

--- python_pip/tasks.py
+++ python_pip/tasks.py
@@ -31,6 +31,7 @@ def list_packages():
         "black",
         "mypy",
         "xonsh[ptk]",
+        "pygments",
         "jedi",
         "xontrib-readable-traceback",
         "xonsh-docker-tabcomplete",
