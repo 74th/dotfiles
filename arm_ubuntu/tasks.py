5ed14d036a6b441e1e25df85f34ee0f858f75d69
Atsushi Morimoto (74th) <site@74th.tech>
2021-02-14 15:22:56 +0900

    up

--- arm_ubuntu/tasks.py
+++ arm_ubuntu/tasks.py
@@ -2,21 +2,17 @@ from typing import cast
 from invoke import task, Context
 
 ubuntu_pkgs = [
-    "direnv",
     "python3",
     "python3-pip",
-    "peco",
-    "nodejs",
-    "vim",
     "golang-go",
 ]
 
 
 @task
 def install(c):
-    c.run("sudo apt update")
+    c.run("sudo apt-get update")
     pkgs = " ".join(ubuntu_pkgs)
-    c.run("sudo apt install " + pkgs)
+    c.run("sudo apt-get install -y " + pkgs)
 
     if c.run("which pyenv", warn=True).failed:
         c.run("git clone https://github.com/pyenv/pyenv.git ~/.pyenv")
