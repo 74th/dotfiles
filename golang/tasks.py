7c2a1ce71aa4eaa38b88e2ef87b87b779fe1cb99
Atsushi Morimoto (74th) <site@74th.tech>
2021-02-20 23:01:26 +0900

    up

--- golang/tasks.py
+++ golang/tasks.py
@@ -2,6 +2,7 @@ import os
 import invoke
 import yaml
 import tempfile
+import detect
 from invoke import task, Context
 
 
@@ -20,15 +21,25 @@ def download_packages(c):
 
 
 @task
-def install_ubuntu(c):
+def install_go(c):
     version = c.run("curl https://golang.org/VERSION?m=text").stdout.strip()
-    #version = "1.14.15"
+    # version = "1.14.15"
+    cpu = c.run("uname -p").stdout.strip()
     if c.run("go version", warn=True).stdout.count(version) > 0:
         return
     with tempfile.TemporaryDirectory() as d:
         with c.cd(d):
-            tar_gz = f"{version}.linux-amd64.tar.gz"
-            c.run(f"wget https://dl.google.com/go/{tar_gz}")
+            if detect.mac:
+                if cpu == "arm":
+                    tar_gz = f"{version}.darwin-arm64.tar.gz"
+                else:
+                    tar_gz = f"{version}.darwin-amd64.tar.gz"
+            else:
+                if cpu == "aarch64":
+                    tar_gz = f"{version}.linux-arm64.tar.gz"
+                else:
+                    tar_gz = f"{version}.linux-amd64.tar.gz"
+            c.run(f"curl -LO https://dl.google.com/go/{tar_gz}")
             c.run(f"sudo rm -rf /usr/local/go")
             c.run(f"sudo tar -C /usr/local -xzf {tar_gz}")
             c.run(f"sudo ln -fs /usr/local/go/bin/* /usr/local/bin/")
