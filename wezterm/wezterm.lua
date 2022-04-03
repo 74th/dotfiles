3e099ba724b30395b84fda3b350a81a7b937f947
Atsushi Morimoto (74th) <site@74th.tech>
2022-04-03 19:21:10 +0900

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,8 +1,53 @@
 local wezterm = require 'wezterm';
-return {
-  color_scheme = "Monokai Remastered",
-  font = wezterm.font_with_fallback({
-    "Liga InputMono",
-    "Input Mono",
-  }),
+
+local c = {}
+local keys = {}
+local launch_menu = {}
+
+table.insert(keys, {key="t", mods="CTRL", action="ShowLauncher"})
+
+table.insert(launch_menu, {
+  label = "bash",
+  args = {"/usr/bin/bash"},
+})
+table.insert(launch_menu, {
+  label = "xonsh",
+  args = {"xonsh"},
+})
+table.insert(launch_menu, {
+  label = "lewill",
+  args = {"ssh", "lewill", "xonsh"},
+  domain = {DomainName="local"},
+})
+
+local ssh_domains = {}
+
+table.insert(ssh_domains, {
+  name = "lewill",
+  remote_address = "lewill",
+  username = "nnyn",
+})
+
+c.window_decorations = "RESIZE"
+
+c.window_padding = {
+  left = 3,
+  right = 3,
+  top = 3,
+  bottom = 3,
 }
+
+c.color_scheme = "Monokai Remastered"
+
+c.font = wezterm.font_with_fallback({
+  "Liga InputMono",
+  "Input Mono",
+})
+c.font_size = 10.5
+
+c.default_prog = {"xonsh"}
+c.launch_menu = launch_menu
+c.keys = keys
+c.ssh_domains = ssh_domains
+
+return c
