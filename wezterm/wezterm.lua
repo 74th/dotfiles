e1aa369c6c59e189f5968c11e1cabb7f8f223dd4
74th <site@74th.tech>
2022-04-05 09:14:05 +0900

    try wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,19 @@
 local wezterm = require 'wezterm';
+
+launch_menu = {}
+
+table.insert(launch_menu, {
+  label = "bash",
+  args = {"/usr/local/bin/bash"},
+})
+table.insert(launch_menu, {
+  label = "xonsh",
+  args = {"/usr/local/bin/xonsh"},
+})
+
 return {
+  launch_menu = launch_menu,
+  -- default_prog = {"/usr/local/bin/xonsh"},
   color_scheme = "Monokai Remastered",
   font = wezterm.font_with_fallback({
     "Liga InputMono",
