3cbdc187c1c527d411bb1e947773a526f708cdbd
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2023-07-15 17:58:34 +0900

    kill-peco

--- ubuntu/install_pyenv.sh
+++ ubuntu/install_pyenv.sh
@@ -1,6 +1,4 @@
 #!env bash
 set -xe
-sudo apt update; sudo apt install -y build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev curl \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt update; sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
