3cecce2215d69b50e19b5509456544bec1c81b53
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-07-12 22:47:27 +0900

    awscli

--- ubuntu/install/awscli.sh
+++ ubuntu/install/awscli.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 set -xe
+rm -rf /tmp/awscli
 mkdir /tmp/awscli
 cd /tmp/awscli
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+elif [ "$ARCH" = "aarch64" ]; then
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
 unzip awscliv2.zip
-sudo ./aws/install
+if [ -f /usr/local/bin/aws ]; then
+    sudo ./aws/install --update
+else
+    sudo ./aws/install
+fi
+
+rm -rf /tmp/awscli
