ce4b77aae70023f7f4ee1d393381f84905173eb9
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-01-11 03:58:14 +0000

    add uv

--- install_full.sh
+++ install_full.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -xe
 if [ -e /etc/debian_version ]; then
-    if [ ! -d "/home/nnyn/.rye" ]; then
-        RYE_INSTALL_OPTION="--yes" curl -sSf https://rye.astral.sh/get | bash
+    if [ ! -f "$HOME/.local/bin/uv" ]; then
+        curl -LsSf https://astral.sh/uv/install.sh | sh
     fi
 
-    if ! type rye >/dev/null 2>&1; then
-        export PATH=$HOME/.rye/shims:$PATH
+    if ! type uv >/dev/null 2>&1; then
+        export PATH=$HOME/.local/bin:$PATH
     fi
 fi
 
@@ -16,6 +16,6 @@ if [ ! -e ~/ghq/github.com/74th/dotfiles ]; then
 fi
 cd ~/ghq/github.com/74th/dotfiles
 
-~/.rye/shims/rye sync
+~/.local/bin/uv sync
 
-~/.rye/shims/python -u -m invoke install
+~/.local/bin/uv run invoke install
