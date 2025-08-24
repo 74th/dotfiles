f785fb6111c10caaeed1a541c00879ba256deac2
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-08-24 15:26:35 +0000

    devcontainer.shに終了コードと相対パスを表示する関数を追加

--- bashrc/devcontainer.sh
+++ bashrc/devcontainer.sh
@@ -8,3 +8,28 @@ set -o vi
 alias ll="ls -alh"
 alias g="git"
 alias gt="git status"
+
+function __show_exitcode() {
+    _EXITCODE=$?
+    if [ $_EXITCODE -ne 0 ]; then echo -e "\e[31m$_EXITCODE\e[m "; fi
+}
+
+function __show_relative_path() {
+    local current_dir=$(pwd)
+    # /workspaces/で始まる場合の処理
+    if [[ "$current_dir" == /workspaces/* ]]; then
+        # /workspaces/projectname/sub/path -> ./sub/path のように変換
+        # /workspaces/projectname -> ./ のように変換
+        local relative_path=$(echo "$current_dir" | sed 's|^/workspaces/[^/]*||')
+        if [[ -z "$relative_path" ]]; then
+            echo "."
+        else
+            echo ".${relative_path}"
+        fi
+    else
+        # /workspaces/以外の場合は通常のパス表示
+        echo "$current_dir"
+    fi
+}
+
+PS1='$(__show_exitcode)$(__show_relative_path) $ '
