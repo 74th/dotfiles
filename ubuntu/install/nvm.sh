f730590c70139a72ba7d5a808459def8035a9896
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2026-08-01 09:26:03 +0900

    nvm

---
+++ ubuntu/install/nvm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.6/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+# ltsをインストール
+nvm use --lts
+nvm alias default lts/*
